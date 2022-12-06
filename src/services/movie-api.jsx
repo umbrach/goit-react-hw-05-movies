622e0a57d32983230168a77f4e79641b3c489331
Denys Barabanov <umbrach415@gmail.com>
2022-12-06 17:32:00 +0200

    Update movie-api.jsx

--- src/services/movie-api.jsx
+++ src/services/movie-api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'api_key=ec03e9e57e4c2ad62d7d835cb4534f63';
+const API_KEY = 'api_key=5168645d33b20991b37ed02c7beb1d13';
 
 export async function getPopularMovie() {
     return await axios.get(
@@ -8,27 +8,28 @@ export async function getPopularMovie() {
   );
 }
 export async function searchMovies(searchQuery) {
-  const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
-  const API_KEY = 'api_key=ec03e9e57e4c2ad62d7d835cb4534f63';
   return await axios.get(
-    `${BASE_URL}?${API_KEY}&query=${searchQuery}&language=en-US`
+    `https://api.themoviedb.org/3/search/movie?${API_KEY}&query=${searchQuery}&language=en-US`
   );
 }
 
 export async function getMovieDetails(id) {
-  const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-  const API_KEY = 'api_key=ec03e9e57e4c2ad62d7d835cb4534f63';
-  return await axios.get(`${BASE_URL}${id}?${API_KEY}&language=en-US`);
+  // const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+  return await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?${API_KEY}&language=en-US`
+  );
 }
 
 export async function getCastDetails(id) {
-  const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-  const API_KEY = 'api_key=ec03e9e57e4c2ad62d7d835cb4534f63';
-  return await axios.get(`${BASE_URL}${id}/credits?${API_KEY}&language=en-US`);
+  // const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+  return await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}/credits?${API_KEY}&language=en-US`
+  );
 }
 
 export async function getMovieReviews(id) {
-  const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-  const API_KEY = 'api_key=ec03e9e57e4c2ad62d7d835cb4534f63';
-  return await axios.get(`${BASE_URL}${id}/reviews?${API_KEY}&language=en-US`);
+  // const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+  return await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}/reviews?${API_KEY}&language=en-US`
+  );
 }
